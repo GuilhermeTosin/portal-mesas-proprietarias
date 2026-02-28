156d11939622bbed6f808e84ffc174dfa9053600
GuilhermeTosin <contact@guilhermetosin.com>
2026-02-28 14:12:41 -0500

    chore: actual hybrid config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-  output: 'server',
-  adapter: node({
+  site: 'https://guilhermetosin.github.io',
+  base: '/portal-mesas-proprietarias',
+  output: isGithubActions ? 'static' : 'server',
+  adapter: isGithubActions ? undefined : node({
     mode: 'standalone',
   }),
   integrations: [tailwind()],
